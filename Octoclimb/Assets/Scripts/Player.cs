57e9badd8e9b4b9299ee9bd783c814ec61d34cd3
Mikko Jyrkäs <mikkojyrkas1@kamk.fi>
2022-09-19 16:02:45 +0300

    spiin

--- Octoclimb/Assets/Scripts/Player.cs
+++ Octoclimb/Assets/Scripts/Player.cs
@@ -6,11 +6,13 @@ public class Player : MonoBehaviour
 {
     //when spinning
     private bool spinning;
+    private bool spinnable;
     [SerializeField] Rigidbody2D RGB;
     private HingeJoint2D HJ;
     [SerializeField] float maxSpeed;
 
     private Vector2 Pole;
+    private Vector2 Dir;
 
     private void Start()
     {
@@ -18,26 +20,8 @@ public class Player : MonoBehaviour
         HJ = GetComponent<HingeJoint2D>();
     }
 
-    private void Update()
+    private void SpinMe()
     {
-        if (Input.anyKey)
-        {
-            if (!spinning)
-            {
-                transform.position = Pole + Vector2.up;
-                transform.rotation = Quaternion.identity;
-                HJ.anchor = new Vector3(Pole.x, Pole.y, 0) - transform.position;
-                spinning = true;
-                HJ.enabled = true;
-                //start ´the spin´
-            }
-        }
-        else
-        {
-            spinning = false;
-            HJ.enabled = false;
-        }
-
         if (spinning)
         {
             if (RGB.velocity.magnitude > maxSpeed)
@@ -46,15 +30,81 @@ public class Player : MonoBehaviour
             }
             else
             {
-                RGB.AddRelativeForce(Vector2.left);
+                RGB.AddRelativeForce(Dir);
             }
         }
+    }
+
+    private void Update()
+    {
+        //mobile
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+
+            if (!spinning && spinnable)
+            {
+                transform.position = Pole + Vector2.up;
+                transform.rotation = Quaternion.identity;
+                HJ.anchor = new Vector3(Pole.x, Pole.y, 0) - transform.position;
+                spinning = true;
+                HJ.enabled = true;
+                spinnable = false;
+                RGB.simulated = true;
+                //start ´the spin´
+            }
+
+            //Which side clicked
+            if (touch.position.x < Screen.width / 2)
+            {
+                Dir = Vector2.left;
+            }
+            else if (touch.position.x > Screen.width / 2)
+            {
+                Dir = Vector2.right;
+            }
+        }
+        //pc
+        else if (Input.GetMouseButton(0))
+        {
+            if (!spinning && spinnable)
+            {
+                transform.position = Pole + Vector2.up;
+                transform.rotation = Quaternion.identity;
+                HJ.anchor = new Vector3(Pole.x, Pole.y, 0) - transform.position;
+                spinning = true;
+                HJ.enabled = true;
+                spinnable = false;
+                RGB.simulated = true;
+                //start ´the spin´
+            }
+
+            //Which side clicked
+            if (Input.mousePosition.x < Screen.width/2)
+            {
+                Dir = Vector2.left;
+            }
+            else if (Input.mousePosition.x > Screen.width / 2)
+            {
+                Dir = Vector2.right;
+            }
+        }
+        else
+        {
+            spinning = false;
+            HJ.enabled = false;
+        }
+
+        //Spin
+        SpinMe();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Pole"))
         {
+            spinnable = true;
+            RGB.simulated = false;
             Pole = collision.transform.position;
             HJ.connectedBody = collision.transform.GetChild(0).GetComponent<Rigidbody2D>();
         }
