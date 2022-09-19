1d8099e82895b79354ad71c5e3cc51713850da08
Mikko Jyrkäs <mikkojyrkas1@kamk.fi>
2022-09-19 15:13:33 +0300

    spin me round round

--- Octoclimb/Assets/Scripts/Player.cs
+++ Octoclimb/Assets/Scripts/Player.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Player : MonoBehaviour
 {
     //when spinning
     private bool spinning;
     [SerializeField] Rigidbody2D RGB;
+    private HingeJoint2D HJ;
     [SerializeField] float maxSpeed;
 
+    private Vector2 Pole;
+
+    private void Start()
+    {
+        RGB = GetComponent<Rigidbody2D>();
+        HJ = GetComponent<HingeJoint2D>();
+    }
 
     private void Update()
     {
@@ -17,13 +24,18 @@ public class Player : MonoBehaviour
         {
             if (!spinning)
             {
+                transform.position = Pole + Vector2.up;
+                transform.rotation = Quaternion.identity;
+                HJ.anchor = new Vector3(Pole.x, Pole.y, 0) - transform.position;
                 spinning = true;
+                HJ.enabled = true;
                 //start ´the spin´
             }
         }
         else
         {
             spinning = false;
+            HJ.enabled = false;
         }
 
         if (spinning)
@@ -36,6 +48,15 @@ public class Player : MonoBehaviour
             {
                 RGB.AddRelativeForce(Vector2.left);
             }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pole"))
+        {
+            Pole = collision.transform.position;
+            HJ.connectedBody = collision.transform.GetChild(0).GetComponent<Rigidbody2D>();
         }
     }
 }
