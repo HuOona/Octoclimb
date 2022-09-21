1b540c22fe8f9eba26ce443c124859b0a7fba532
Mikko Jyrkäs <mikkojyrkas1@kamk.fi>
2022-09-21 15:05:12 +0300

    build

--- Octoclimb/Assets/Scripts/Player.cs
+++ Octoclimb/Assets/Scripts/Player.cs
@@ -32,13 +32,14 @@ public class Player : MonoBehaviour
             }
             else
             {
-                RGB.AddRelativeForce(Dir );
+                RGB.AddRelativeForce(Dir * spinForce *Time.deltaTime);
             }
         }
     }
 
     void startSpin()
     {
+        RGB.velocity = Vector2.zero;
         transform.position = Pole + spinRadius ;
         transform.rotation = Quaternion.identity;
         HJ.anchor = (new Vector3(Pole.x, Pole.y, 0) - transform.position) / transform.localScale.x;
@@ -49,7 +50,7 @@ public class Player : MonoBehaviour
 
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         //mobile
         if (Input.touchCount > 0)
